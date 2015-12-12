1c536a99e7e662f269d46224c0632724182e7812
Carlos Ramos <carlosramos@outlook.com>
2015-12-12 13:58:11 +0100

    Añadida licencia (GPL3)

--- convert.pl
+++ convert.pl
@@ -2,7 +2,18 @@
 
 # Convierte el archivo de microdatos del CIS en un CSV
 
-# <licencia>
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 
 # Descripción de los campos
